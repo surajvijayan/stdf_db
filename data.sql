3b64fa93b058e75c45be29ffba184b9c956b5da0
Suraj Vijayan <suraj.vijayan1966@gmail.com>
2026-05-04 10:05:09 -0700

    u

--- data.sql
+++
@@ -1,6 +0,0 @@
-select TEST_NAME,COND_GROUP_ID,COND,count(id) as count  from  ptest_tdigest_cond_view where STDF_ID=1477 and COND_GROUP_ID=199 and COND='VDDO_V1P8:1.674V' group by TEST_NAME,COND_GROUP_ID;
-
-
-select TEST_NAME,COND_GROUP_ID,COND,count(id) as count  from  ptest_tdigest_cond_view where STDF_ID=1477 and COND_GROUP_ID=199 and COND='VDDO_V1P8:1.674V';
-
-
